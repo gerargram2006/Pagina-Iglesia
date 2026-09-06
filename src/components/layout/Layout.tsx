6e45a6e444426e08651024599e32a9a41e5b6d17
gerargram2006 <gerarcb123@gmail.com>
2026-09-05 22:16:53 -0500

    refactor(frontend): Mover componentes base a la subcarpeta layout/

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,8 @@
-import { Outlet, useLocation } from 'react-router-dom';
+﻿import { Outlet, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Footer from './Footer';
-import ScrollToTop from './ScrollToTop';
-import useScrollAnimations from '../hooks/useScrollAnimations';
+import ScrollToTop from '../ui/ScrollToTop';
+import useScrollAnimations from '../../hooks/useScrollAnimations';
 
 export default function Layout() {
     const { pathname } = useLocation();
@@ -17,9 +17,9 @@ export default function Layout() {
         <>
             {/* Renderiza el contenido de la ruta hija actual */}
             <Outlet />
-            {/* Muestra el pie de página en todas las páginas */}
+            {/* Muestra el pie de pÃ¡gina en todas las pÃ¡ginas */}
             <Footer />
-            {/* Incluye el botón de volver al inicio */}
+            {/* Incluye el botÃ³n de volver al inicio */}
             <ScrollToTop />
         </>
     );
